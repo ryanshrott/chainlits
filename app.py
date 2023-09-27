f3481e4f7a84fcb5ff3c9cabf0050b5cd57e3ae2
Ryan Shrott <ryanshrott@gmail.com>
2023-09-27 12:26:48 -0400

    messages

--- app.py
+++ app.py
@@ -29,14 +29,52 @@ from typing import Optional
 import random
 import string
 from pymongo import MongoClient
-
+import tiktoken
 MAX_ITER = 5
 
 llm = ChatOpenAI(temperature=0)
 client = MongoClient(os.getenv("MONGO_AUTH"))  # Make sure to set the MONGO_CONNECTION_STRING in your .env
 db = client.get_database('smartbids')  # replace 'your_database_name' with your actual database name
 clients_collection = db['leads']
-
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
 
 async def process_new_delta(new_delta, openai_message, content_ui_message, function_ui_message):
     if "role" in new_delta:
@@ -299,15 +337,20 @@ async def run_conversation(user_message: str):
     print(message_history[1:])
     #message_history = [message for message in message_history if message["role"] != "function"]
     cur_iter = 0
+
+    
     while cur_iter < MAX_ITER:
 
         # OpenAI call
         openai_message = {"role": "", "content": ""}
         function_ui_message = None
         content_ui_message = cl.Message(content="")
+        messages_model = message_history[:2] + message_history[max(2, len(message_history) - N):]
+        token_count = num_tokens_from_messages(messages_model)
+        print('token_count', token_count)
         async for stream_resp in await openai.ChatCompletion.acreate(
-            model="gpt-3.5-turbo-16k-0613",
-            messages=message_history[:2] + message_history[max(2, len(message_history) - N):],
+            model="gpt-3.5-turbo-16k-0613" if token_count > 3200 else "gpt-3.5-turbo-0613",
+            messages=messages_model,
             stream=True,
             function_call="auto",
             functions=functions,
